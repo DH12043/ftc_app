ec048ceb2bc9ed713756ce8591bac03311f60425
Daniel Olsson <dmolsson@outlook.com>
2019-06-22 15:39:58 -0500

    Voltage sensor sample finshed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltageMonitoring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltageMonitoring.java
@@ -2,15 +2,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 @TeleOp
 public class VoltageMonitoring extends OpMode {
 
-    public void init() {
+    private VoltageSensor _batteryVoltage;
 
+    public void init() {
+        _batteryVoltage = hardwareMap.voltageSensor.get("Expansion Hub 2");
     }
 
     public void loop() {
 
+        double voltage = _batteryVoltage.getVoltage();
+
+        telemetry.addData("BatteryVoltage", "%.3f", voltage);
     }
 }
